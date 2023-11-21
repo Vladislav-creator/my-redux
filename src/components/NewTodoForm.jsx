0cd7d51f06889a4d5163b6e2558b7b3f4fca900c
Vladislav-creator <130439019+Vladislav-creator@users.noreply.github.com>
2023-11-21 20:38:31 +0300

    4

--- src/components/NewTodoForm.jsx
+++ src/components/NewTodoForm.jsx
@@ -1,12 +1,13 @@
+import './App.css';
 const NewTodoForm = ({ value, updateText, handleAction }) => {
     return (
       <label>
-        <input
+        <input  className='InputChange' 
           placeholer='new todo'
           value={value}
           onChange={(e) => updateText(e.target.value)}
         />
-        <button onClick={handleAction}>Add todo</button>
+        <button className='ButtonChange' onClick={handleAction}>Add todo</button>
       </label>
     );
   };
